316635ce6bf60a92fd0d9d6e91fc6127f25bac35
Anas Ahmed <anasahmed700@gmail.com>
2020-06-18 02:53:48 +0500

    modified DRF api views using classes

--- newsapi/news/api/urls.py
+++ newsapi/news/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import article_list_create_api_view, article_detail_api_view
+# from .views import article_list_create_api_view, article_detail_api_view
+from .views import ArticleListCreateView, ArticleDetailAPIView
+
 
 urlpatterns = [
-    path('articles/', article_list_create_api_view, name='article-list'),
-    path('articles/<int:pk>', article_detail_api_view, name='article-detail')
+    # path('articles/', article_list_create_api_view, name='article-list'),
+    # path('articles/<int:pk>', article_detail_api_view, name='article-detail')
+
+    path('articles/', ArticleListCreateView.as_view(), name='article-list'),
+    path('articles/<int:pk>', ArticleDetailAPIView.as_view(), name='article-detail')
 ]
--- newsapi/news/api/views.py
+++ newsapi/news/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from ..models import Article
 from .serializers import ArticleSerializer
 
@@ -18,6 +21,7 @@ def article_list_create_api_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(["Get", "PUT", "DELETE"])
 def article_detail_api_view(request, pk):
@@ -39,5 +43,45 @@ def article_detail_api_view(request, pk):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "DELETE":
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ArticleListCreateView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.filter(active=True)
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetailAPIView(APIView):
+
+    def get_object(self, pk):
+        article = get_object_or_404(Article, pk=pk)
+        return article
+
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        article = self.get_object(pk)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
