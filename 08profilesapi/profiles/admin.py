5dc1742f4a1598db354e7fcbbe9e44efba0fcc16
Anas Ahmed <anasahmed700@gmail.com>
2020-06-19 18:45:48 +0500

    initialize & setup models for profilesapi project

---
+++ 08profilesapi/profiles/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Profile, ProfileStatus
+
+
+# Register your models here.
+admin.site.register(ProfileStatus)
+admin.site.register(Profile)
---
+++ 08profilesapi/profiles/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=240, blank=True)
+    city = models.CharField(max_length=40, blank=True)
+    avatar = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class ProfileStatus(models.Model):
+    user_profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    status_content = models.CharField(max_length=240)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'statuses'
+
+    def __str__(self):
+        return self.user_profile
