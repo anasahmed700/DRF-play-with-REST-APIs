e46e2b9741e646a99d9ae4128c6230ac8924734f
Anas Ahmed <anasahmed700@gmail.com>
2020-06-20 16:59:50 +0500

    initialize VueJS project

---
+++ VueJS/01intro/main.js
@@ -0,0 +1,9 @@
+var app1 = new Vue({
+    el:  '#app',
+    data: {
+        message: "Hello, World!",
+        value: 5,
+        imgSrc: 'https://www.nasa.gov/sites/default/files/thumbnails/image/pillars_of_creation.jpg',
+        link: 'https://vuejs.org/'        
+    }
+})
